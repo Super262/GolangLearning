2afc212c2e09f0b2fb182d84994273f6a899d6df
Super262 <virusformail@gmail.com>
2020-10-12 16:56:50 +0800

    Assignment 2

--- main/main.go
+++ main/main.go
@@ -1,7 +1,7 @@
 package main
 
-import "fmt"
+import ()
 
 func main() {
-	fmt.Println("Hello, world!")
+
 }
--- main/trunc.go
+++ main/trunc.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
-	NumFloat := ""
-	fmt.Println("Enter a floating point number. ")
-	fmt.Scan(&NumFloat)
-	FloatingPart := strings.Split(NumFloat, ".")[1]
-	NumFloat = "0." + FloatingPart
-	fmt.Println(NumFloat)
+
 }
