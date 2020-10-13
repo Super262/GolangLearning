c2f7920ba30435f6a78f28a1f267b0677b4cc43a
Super262 <virusformail@gmail.com>
2020-10-13 20:24:47 +0800

    Assignment 3

--- main/main.go
+++ main/main.go
@@ -18,11 +18,23 @@ func main() {
 	slice2 = append(slice2, 100)
 	fmt.Printf("Capacity of slice1: %d, length of slice1: %d.\n", cap(slice2), len(slice2))
 	fmt.Println(slice2)
+
 	idMap := make(map[string]int)
 	idMap["Joe"] = 456
 	idMap["Green"] = 123
 	fmt.Printf("Joe's ID is %d.\n", idMap["Joe"])
 	fmt.Printf("Green's ID is %d.\n", idMap["Green"])
 	delete(idMap, "Joe")
-	fmt.Printf("Joe's ID is %d.\n", idMap["Joe"])
+	id, p := idMap["Joe"]
+	fmt.Println(p)
+	fmt.Printf("Joe's ID is %d.\n", id)
+	idMap["Joe"] = 456
+	idMap["Green"] = 123
+	idMap["Peter"] = 789
+	idMap["Ann"] = 012
+	idMap["Jane"] = 345
+	idMap["Lisa"] = 678
+	for key, value := range idMap {
+		fmt.Println(key, value)
+	}
 }
---
+++ main/slice.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func less(i int, j int) bool {
+	if i < j {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	slice := make([]int, 0, 3)
+	var input string = "0"
+	for {
+		fmt.Println("Enter an integer. Enter X to quit.")
+		_, _ = fmt.Scan(&input)
+		if strings.Compare(input, "X") == 0 {
+			break
+		}
+		val, _ := strconv.Atoi(input)
+		slice = append(slice, val)
+		sort.Ints(slice)
+		fmt.Println(slice)
+	}
+}
