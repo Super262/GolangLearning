c5633eb0fbff9aa479a27fc838ba2126d8b3fed8
Super262 <virusformail@gmail.com>
2020-12-26 20:54:05 +0800

    New Assignment

--- main/race.go
+++ main/race.go
@@ -2,25 +2,28 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 )
 
-func printAll(counter *int, name string, idle int) {
+func setCounter(counter *int, idle int) {
 	for i := 0; i < 10; i++ {
-		*counter = *counter + 1
-		fmt.Println("Goroutine " + name + ": " + strconv.Itoa(*counter))
+		value := *counter
 		time.Sleep(time.Duration(idle) * time.Millisecond)
+		value++
+		*counter = value
 	}
 }
 
 func main() {
 	var counter int
 	counter = 0
-	go printAll(&counter, "1", 125)
-	go printAll(&counter, "2", 250)
+	go setCounter(&counter, 125)
+	go setCounter(&counter, 250)
 	time.Sleep(5000 * time.Millisecond)
+	fmt.Printf("Final Value of Counter: %d\n", counter)
 }
 
-// These two threads are executing the similar code from the start respectively.
-// Although the final result is proper, a race condition occurs during the whole process.
+// Explanations:
+// These two threads are executing the same code from the start, respectively.
+// The final result, which should be 20, is 10. The final result is wrong because a race condition occurs during the whole process.
+// Every query would get the old value of counter during the whole process because these two threads are performing write operations without any limits.
